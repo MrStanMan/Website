e0521fd3ed1b098a0c1f9c1afe7f75295e69d1e5
Stan <stan@fizz.nl>
2019-02-11 11:48:00 +0100

    Initial commit

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContactController extends AbstractController
+{
+    public function contact(Request $request, \Swift_Mailer $mailer)
+    {
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $contactFormData = $form->getData();
+
+            $message = (new \Swift_Message('Er is contact opgenomen'))
+                ->setFrom($contactFormData['email'])
+                ->setTo('stan@fizz.nl')
+                ->setBody(
+                    $contactFormData['message'],
+                    'text/plain'
+                );
+
+            $mailer->send($message);
+
+            $this->addFlash('success', 'Wij hebben uw verzoek ontvangen.');
+
+            return $this->redirectToRoute('contact');
+        }
+
+
+        return $this->render('contact/contact.html.twig', [
+            'contact_form' => $form->createView()
+        ]);
+    }
+}
---
+++ src/Controller/PageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PageController extends AbstractController
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index()
+    {
+        return $this->render('pages/index.html.twig');
+    }
+}
