0206d8bc75e198a7abebbe9e494b25e9a50a9206
Stan <stan@fizz.nl>
2019-02-11 15:14:19 +0100

    Login
    - Login via een geregistreerd account
    - Persoon kan ook uitloggen
    - Menu wordt aangepast op het moment dat er ingelogd is.

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -22,11 +22,11 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Voer een wachtwoord in alstublieft',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Het wachtwoord moet minstens {{ limit }} tekens lang zijn',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
